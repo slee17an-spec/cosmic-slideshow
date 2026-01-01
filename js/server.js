e5cea8c15c8cdab15592b3713e4dda9c66b9c0df
Syamsul Bachri <slee17an@gmail.com>
2026-01-02 01:38:08 +0800

    update fitur kosmik

--- js/server.js
+++ js/server.js
@@ -1,17 +1,27 @@
-import express from "express";
-import fetch from "node-fetch";
+const express = require("express");
+const axios = require("axios");
 
+const url = "https://api.nasa.gov/planetary/apod?api_key=6VIg0kOoD2PRkZIKmruMwiNa9jYGJLdRBQiH3aOd";
+console.log("NASA_KEY:", process.env.NASA_KEY);
 const app = express();
 
-// Endpoint proxy NASA APOD
 app.get("/api/nasa", async (req, res) => {
   try {
-    const nasaRes = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_KEY}`);
-    const data = await nasaRes.json();
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_KEY}`;
+    console.log("Fetching:", url);
+
+    const nasaRes = await axios.get(url); // ✅ axios handle HTTPS dengan baik
+    console.log("Status:", nasaRes.status);
+
+    const data = nasaRes.data;
+    console.log("DATA:", data);
+
     res.json(data);
   } catch (err) {
-    res.status(500).json({ error: "Failed to fetch NASA APOD" });
-  }
+  console.error("Error fetching NASA:", err); // log seluruh objek error
+  res.status(500).json({ error: "Failed to fetch NASA APOD" });
+}
+
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
